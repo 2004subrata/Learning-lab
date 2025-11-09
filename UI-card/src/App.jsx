204ce8c81f767bbb3e3282fd61d1360318d46c92
Subrata Mahato <2004subratamahato@gmail.com>
2025-11-10 01:11:23 +0530

    delete package-lock.json

--- UI-card/src/App.jsx
+++ UI-card/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div className='flex justify-center'>
-      <h1 className='text-2xl text-white bg-slate-500 m-2 px-4 py-2 rounded-2xl'>hey from Subrata...!</h1>
+      <h1 className='text-2xl text-white bg-slate-600 m-2 px-4 py-2 rounded-2xl'>hey from Subrata...!</h1>
     </div>
   )
 }
